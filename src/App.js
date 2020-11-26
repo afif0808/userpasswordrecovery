9bb70fe7b3a7b77fa429f3ac75e4e77c4ec1f7d7
afif0808 <afif.panai@gmail.com>
2020-11-26 08:43:08 +0700

    another commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,175 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+
+import 'react-activity-indicator/src/activityindicator.css'
+import ActivityIndicator from 'react-activity-indicator'
+async function RecoverUserPassword({code , password}) {
+  const req = {
+    method : "POST",
+    headers : {
+      "Content-Type" : "application/json",
+    },
+    body : JSON.stringify({code : code , password : password})
+  }
+  
+  const resp = await fetch("http://localhost:777/users/password/recover",req)
+
+  if(!resp.ok) {
+    console.log(resp)
+    throw Error
+  }
+  
+  return await resp.json()
+}
+
+
+function DotsLoader({dotCountLimit}) {
+  const [dotCount , setDotCount] = useState(0)  
+  useEffect(()=>{
+    setInterval(()=>{
+      if(dotCount < dotCountLimit) {
+        setDotCount(dotCount+1)
+      } else {
+        setDotCount(1)
+      }
+    },1000)
+  },[])
+
+  function renderDots() {
+    var dots = []
+    for(let i = 0 ; i < dotCount ; i++) {
+      dots.push(
+        <span key={i} style={{marginRight:5,backgroundColor:"#f00",width:30,height:30,borderRadius:30}}>WoW</span>
+      )
+    }
+    return dots
+  }
+
+  return (
+    <div>
+      {renderDots()}
+    </div>
+  )
+
+}
+
 
 function App() {
+  const [passwordOne ,setPasswordOne] = useState("")
+  const [passwordTwo , setPasswordTwo] = useState("")
+  const [error , setError] = useState("")
+  const [success , setSuccess] = useState("")
+  const [loading , setLoading] = useState(false)
+  function onSubmit(e) {
+    e.preventDefault()
+    setSuccess("")
+    setError("")
+    if(!!passwordTwo) {
+      if(passwordOne != passwordTwo) {
+        setError("Kata kunci tidak sama")
+        return
+      } else {
+        setError("")
+      }
+    }
+    
+    var url = new URL(window.location.href)
+    var code = url.searchParams.get("code");
+    console.log(code)
+    setLoading(true)
+
+    RecoverUserPassword({code : code , password : passwordOne}).then((resp)=>{
+      console.log("Woi",resp)
+      setSuccess("Kata sandi berhasil diganti")
+      setLoading(false)
+      // setTimeout(()=>{setSuccess("")},3000)
+    }).catch((error)=>{
+      setError("Kata sandi gagal diganti")
+      setLoading(false)
+    })
+  
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={"col-12"}>
+        <div className={"col-lg-6"} style={{margin : "20px auto",border : "solid 1px #ccc",padding:20}}>
+            <center><h1>Atur ulang kata sandi</h1></center>
+            <form onSubmit={onSubmit}>
+              <table className={"table"}>
+                <tr>
+                  <td>Kata sandi</td>
+                  <td>
+                    <input 
+                      onChange={(e)=>{
+                        setPasswordOne(e.target.value)
+                      }}
+                      required={true} 
+                      type={"password"} 
+                      className={"form-control"} />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Masukkan ulang kata sandi</td>
+                  <td>
+                    <input
+                      onChange={(e)=>{
+                        setPasswordTwo(e.target.value)
+                        if(!!e.target.value) {
+                          if(passwordOne != e.target.value) {
+                            setError("Kata kunci tidak sama")
+                          } else {
+                            setError("")
+                          }
+                        }
+                      }}                     
+                      required={true} 
+                      type={"password"} 
+                      className={"form-control"} />
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan={2}>
+                    <button className={"btn btn-primary"}>SIMPAN</button>
+                    <br/>
+                    <br/>
+
+                    {!!error && 
+                        <span className={"btn btn-danger"}>{error}</span>
+                    } 
+                    {!!loading &&
+                      <ActivityIndicator
+                      number={5}
+                      diameter={0}
+                      borderWidth={1}
+                      duration={200}
+                      activeColor="#999"
+                      borderColor="white"
+                      color={"#000"}
+                      borderWidth={5}
+                      borderRadius="50%" 
+                      />
+                    }
+
+                    {!!success &&
+                      <span className={"btn btn-success"}>{success}</span>
+                    }
+
+
+                  </td>
+                </tr>
+              </table>
+            </form>
+         
+        </div>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
